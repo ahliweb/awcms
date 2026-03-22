91c247d092ab25f13bd34435b531afa127d1897b
unggul <timako@arung.net>
2026-03-22 12:10:51 +0700

    chore: Update drift and flutter_riverpod dependencies, regenerating database DAOs.

--- awcms-mobile/primary/lib/core/database/daos/blogs_dao.g.dart
+++ awcms-mobile/primary/lib/core/database/daos/blogs_dao.g.dart
@@ -5,4 +5,12 @@ part of 'blogs_dao.dart';
 // ignore_for_file: type=lint
 mixin _$BlogsDaoMixin on DatabaseAccessor<AppDatabase> {
   $LocalBlogsTable get localBlogs => attachedDatabase.localBlogs;
+  BlogsDaoManager get managers => BlogsDaoManager(this);
+}
+
+class BlogsDaoManager {
+  final _$BlogsDaoMixin _db;
+  BlogsDaoManager(this._db);
+  $$LocalBlogsTableTableManager get localBlogs =>
+      $$LocalBlogsTableTableManager(_db.attachedDatabase, _db.localBlogs);
 }
